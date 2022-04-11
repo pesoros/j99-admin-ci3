9c81b9e63dcdb0df836ba420b5c4a7bcd84d6dc2
bayuyuhartono <bayuyuhartono@gmail.com>
2022-04-11 23:35:37 +0700

    t

--- application/modules/trip/controllers/Route.php
+++ application/modules/trip/controllers/Route.php
@@ -36,14 +36,14 @@ class Route extends MX_Controller
         $this->form_validation->set_rules('status', display('status'), 'required');
         $start = $this->db->select('*')->from('trip_location')->where('id', $this->input->post('start_point'))->get()->row();
 
-        $end = $this->db->select('*')->from('trip_location')->where('id', $this->input->post('end_point'))->get()->row();
-        $array1 = array(isset($start->name) ? $start->name : '', isset($end->name) ? $end->name : '');
-        $array2 = $this->input->post('routeList') != null ? $this->input->post('routeList') : '';
-        $array3 = array_diff($array2, $array1);
-        $output = implode(',', $array3);
-        $stoppagestring = implode(',', $array2);
+        // $end = $this->db->select('*')->from('trip_location')->where('id', $this->input->post('end_point'))->get()->row();
+        // $array1 = array(isset($start->name) ? $start->name : '', isset($end->name) ? $end->name : '');
+        // $array2 = $this->input->post('routeList') != null ? $this->input->post('routeList') : '';
+        // $array3 = array_diff($array2, $array1);
+        // $output = implode(',', $array3);
+        // $stoppagestring = implode(',', $array2);
 
-        $stopagp = (!empty($stoppagestring) ? $stoppagestring : '');
+        // $stopagp = (!empty($stoppagestring) ? $stoppagestring : '');
 
 		$pickup = [];
 		$drop = [];
