2ef1ee1286c513b6af298dda6131a80a8be0d134
bayuyuhartono <bayuyuhartono@gmail.com>
2022-04-20 11:02:49 +0700

    ttt

--- application/config/database.php
+++ application/config/database.php
@@ -74,35 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 $active_record = TRUE;//ci version 2.x
 
-// $db['default'] = array(
-//     'dsn'   => '',
-//     'hostname' => '127.0.0.1',
-//     'username' => 'u1442932_pesorosdev',
-//     'password' => 'westlondon05',
-//     'database' => 'u1442932_juragansem',
-//     'dbdriver' => 'mysqli',
-//     'dbprefix' => '',
-//     'pconnect' => FALSE,
-//     'db_debug' => (ENVIRONMENT !== 'development'),
-//     'cache_on' => FALSE,
-//     'cachedir' => '',
-//     'char_set' => 'utf8',
-//     'dbcollat' => 'utf8_general_ci',
-//     'swap_pre' => '',
-//     'encrypt'  => FALSE,
-//     'compress' => FALSE,
-//     'autoinit' => TRUE,//ci version 2.x
-//     'stricton' => FALSE,
-//     'failover' => array(),
-//     'save_queries' => TRUE
-// );
- 
 $db['default'] = array(
     'dsn'   => '',
     'hostname' => '127.0.0.1',
-    'username' => 'root',
-    'password' => '',
-    'database' => 'juragansem',
+    'username' => 'u1442932_pesorosdev',
+    'password' => 'westlondon05',
+    'database' => 'u1442932_juragansem',
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
     'pconnect' => FALSE,
@@ -119,3 +96,26 @@ $db['default'] = array(
     'failover' => array(),
     'save_queries' => TRUE
 );
+ 
+// $db['default'] = array(
+//     'dsn'   => '',
+//     'hostname' => '127.0.0.1',
+//     'username' => 'root',
+//     'password' => '',
+//     'database' => 'juragansem',
+//     'dbdriver' => 'mysqli',
+//     'dbprefix' => '',
+//     'pconnect' => FALSE,
+//     'db_debug' => (ENVIRONMENT !== 'development'),
+//     'cache_on' => FALSE,
+//     'cachedir' => '',
+//     'char_set' => 'utf8',
+//     'dbcollat' => 'utf8_general_ci',
+//     'swap_pre' => '',
+//     'encrypt'  => FALSE,
+//     'compress' => FALSE,
+//     'autoinit' => TRUE,//ci version 2.x
+//     'stricton' => FALSE,
+//     'failover' => array(),
+//     'save_queries' => TRUE
+// );
